53587c465d3ec72f273304d7a4db982c74330ce9
MBA <matheus-araujo1998@hotmail.com>
2020-12-04 14:34:25 -0300

    Little modifications

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,10 @@
 
 int main(int argc, char *argv[])
 {
+	int valor;
 	std::cout << "Hello world!" << std::endl;
+	std::cout << "write a number please" << std::endl;
+	std::cin >> valor;
+	std::cout << valor << std::endl;
+	std::cout << "Im starting to understand" << std::endl;
 }
