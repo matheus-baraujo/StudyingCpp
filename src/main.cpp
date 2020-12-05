571e2038e167b75d267d31d9cb9d63ce7d118501
MBA <matheus-araujo1998@hotmail.com>
2020-12-05 14:11:15 -0300

    Relearning how to read full sentences input

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 int main(int argc, char *argv[])
 {
 	int valor;
+	char texto[90];
 	std::cout << "Hello world!" << std::endl;
 	std::cout << "write a number please" << std::endl;
 	std::cin >> valor;
@@ -11,6 +12,9 @@ int main(int argc, char *argv[])
 	{
 		std::cout << valor << std::endl;
 		std::cout << "ok, i already understood" << std::endl;
+		std::cout << "Please write a phrase" << std::endl;
+		std::cin >> texto;
+		std::cout << texto << std::endl;
 	}
 	else
 	{
