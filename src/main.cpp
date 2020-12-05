44cf172f52f84cd82c1431373fc75441f0e050e4
MBA <matheus-araujo1998@hotmail.com>
2020-12-05 16:34:09 -0300

    Making some test calculations with io streams

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 
 int main(int argc, char *argv[])
 {
-	int valor;
+/*	
+	int valor {0};
 	char texto[90];
 	std::cout << "Hello world!" << std::endl;
 	std::cout << "write a number please" << std::endl;
@@ -21,5 +22,30 @@ int main(int argc, char *argv[])
 		std::cout << valor << std::endl;
 	std::cout << "Im starting to understand" << std::endl;	
 	}
+*/
+	const int smallRoomCharge {25};
+	const int bigRoomCharge {35};
+	const float tax {0.06};
+
+	int numberSmallRoom {0};
+	int numberBigRoom {0};
+	int totalCost {0};
+
+// sorry for being a Dnd addict kk
+
+	std::cout << "Hello!! Welcome to Whitestone Glamourous Tavern" << std::endl;
 	
+	std::cout << "How many small rooms would you like to rent?" << std::endl;
+	std::cin >> numberSmallRoom;
+
+	std::cout << "How many big rooms would you like to rent?" << std::endl;
+	std::cin >> numberBigRoom;
+
+	totalCost = (25*numberSmallRoom)+(35*numberBigRoom);
+
+	std::cout << "This would cost you " << totalCost << " gold pieces." << std::endl;
+	std::cout << "With a tax of: " << totalCost*tax << " gold pieces." << std::endl;
+
+	std::cout << "This estimate is valid for 30 days" << std::endl;
+
 }
