03bc2efae2190fc8d22f87338986dd09dad0efea
MBA <matheus-araujo1998@hotmail.com>
2020-12-09 15:19:04 -0300

    Added the section 10 challenge

--- src/main.cpp
+++ src/main.cpp
@@ -184,97 +184,129 @@ int main(int argc, char *argv[])
 
 // Section 9 challenge (create a progam with a loop menu where the user can navigate and explore functions)	
 	
-	int selector {0};
+	// int selector {0};
 
-	int created {0};
-	string character_name {"Not created"};
-	int hp {0};
-	int max_hp {0};
-	int level {0};
-	string character_class {"Not created"};
-	int stats[6] {0};
-	vector <string> classes {"Artificier", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"};
-	int quit {0};
+	// int created {0};
+	// string character_name {"Not created"};
+	// int hp {0};
+	// int max_hp {0};
+	// int level {0};
+	// string character_class {"Not created"};
+	// int stats[6] {0};
+	// vector <string> classes {"Artificier", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"};
+	// int quit {0};
 
-	cout << "Welcome to the DnD character manager!!" << endl;
-	cout << "Please choose what you want to do from the options below" << endl;
-	cout << endl;
-	cout << "1 - Add a new character" << endl;
-	cout << "2 - Edit the character" << endl;
-	cout << "3 - Manage HP" << endl;
-	cout << "4 - Erase character" << endl;
-	cout << "5 - Quit" << endl;
-	cout << endl;
+	// cout << "Welcome to the DnD character manager!!" << endl;
+	// cout << "Please choose what you want to do from the options below" << endl;
+	// cout << endl;
+	// cout << "1 - Add a new character" << endl;
+	// cout << "2 - Edit the character" << endl;
+	// cout << "3 - Manage HP" << endl;
+	// cout << "4 - Erase character" << endl;
+	// cout << "5 - Quit" << endl;
+	// cout << endl;
 
-	cin >> selector;
+	// cin >> selector;
 
-	while (1) //infinite loop
-	{
-		switch (selector)
-		{
-		case 1:
-			cout << "Please enter the character name: ";
-			getline(cin, character_name);
-			cout << endl;
-			cout << "Plese choose a class for your character from the ones listed below:" << endl;
-			for(string classe: classes)
-			{
-				cout << classe << endl;	
-			}
-			getline(cin, character_class);
+	// while (1) //infinite loop
+	// {
+	// 	switch (selector)
+	// 	{
+	// 	case 1:
+	// 		if (created == 0)
+	// 		{
+	// 			cout << "Please enter the character name: ";
+	// 			getline(cin, character_name);
+	// 			cout << endl;
+	// 			cout << "Plese choose a class for your character from the ones listed below:" << endl;
+	// 			for(string classe: classes)
+	// 			{
+	// 				cout << classe << endl;	
+	// 			}
+	// 			getline(cin, character_class);
+
+	// 		}
+	// 		else
+	// 		{
+	// 			cout << "There is a character created already, if you want to create a new character please erase the current one first.";
+	// 		}
 			
-			
+	// 		break;
 
-			break;
+	// 	case 2: // see or edit character
+	// 		/* code */
+	// 		break;
+		
+	// 	case 3: // manage hp
+	// 		cout << "Please, enter your current hp: ";
+	// 		cin >> hp;
+	// 		cout << endl;
+	// 		cout << "HP atualized" << endl; 
+	// 		break;
+		
+	// 	case 4: //erase character
+	// 		if (created == 1)
+	// 		{
+	// 			character_name = "Not created";
+	// 			character_class = "Not created";
+	// 			level = 0;
+	// 			for (int stat: stats)
+	// 			{
+	// 				stat = 0;
+	// 			}
+	// 			hp = 0;
+	// 			max_hp = 0;
+	// 			created = 0;
+	// 		}
+	// 		else
+	// 		{
+	// 			cout << "There is no character created to be erased" << endl;
+	// 		}
+	// 		break;		
+		
+	// 	case 5: //quit
+	// 		quit = 1;
+	// 		break;
+		
+	// 	default:
+	// 		cout << "Sorry, please enter a valid option" << endl;
+	// 		break;
+	// 	}
 
-		case 2: // edit character
-			/* code */
-			break;
-		
-		case 3: // manage hp
-			cout << "Please, enter your current hp: ";
-			cin >> hp;
-			cout << endl;
-			cout << "HP atualized" << endl; 
-			break;
-		
-		case 4: //erase character
-			if (created == 1)
-			{
-				character_name = "Not created";
-				character_class = "Not created";
-				level = 0;
-				for (int stat: stats)
-				{
-					stat = 0;
-				}
-				hp = 0;
-				max_hp = 0;
-				created = 0;
-			}
-			else
-			{
-				cout << "There is no character created to be erased" << endl;
-			}
-			break;		
-		
-		case 5: //quit
-			quit = 1;
-			break;
-		
-		default:
-			cout << "Sorry, please enter a valid option" << endl;
-			break;
-		}
-
-		if (quit == 1)
-		{
-			break;
-		}
-	}
+	// 	if (quit == 1)
+	// 	{
+	// 		break;
+	// 	}
+	// }
 	
 
-// Section 10 challenge and tests	
-	string test;
-	getline(cin,test);
+// Section 10 challenge and tests (simple substitution cypher)	
+	string alphabet {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "};	// important to remeber the spacebar
+	string cypher {"fghijVWXYZpqrstuABCDEabcdeKLMNOvwxyzFGHIJklmnoPQRSTU "};
+
+	string original_text;
+	string transformed_text;
+
+	cout << "Please write a text to be encrypted:" << endl;
+	getline(cin, original_text);
+	transformed_text = original_text;
+
+	cout << endl;
+
+	for(size_t i {0}; i < original_text.size(); i++)
+	{
+		transformed_text[i] = cypher[alphabet.find(original_text[i])];
+	}
+
+	cout << original_text << endl;
+	cout << transformed_text << endl;
+
+	for(size_t i {0}; i < original_text.size(); i++)
+	{
+		transformed_text[i] = alphabet[cypher.find(transformed_text[i])];
+	}
+
+	cout << transformed_text << endl;
+
+	cout << transformed_text.compare(original_text);
 }
